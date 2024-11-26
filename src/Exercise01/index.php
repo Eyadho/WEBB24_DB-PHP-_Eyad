5d093cf2a58e50bf7b9e08f241607af03b27eaca
Eyad <eyad.hosain.2017@gmail.com>
2024-11-26 18:21:32 +0100

    commit

---
+++ src/Exercise01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lektion 1 - Hello World</title>
+</head>
+<body>
+    <h1>Lektion 1 - Hello World</h1>
+
+    <?
+    // Vår första kod i PHP
+    echo 'Hello, World';
+    ?>
+</body>
+</html>
