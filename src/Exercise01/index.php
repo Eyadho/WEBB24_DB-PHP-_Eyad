01c58888b06dfc13db75e5eeda80e3990e277ef7
Eyad <eyad.hosain.2017@gmail.com>
2024-11-26 19:38:03 +0100

    init

--- src/Exercise01/index.php
+++ src/Exercise01/index.php
@@ -12,5 +12,14 @@
     // Vår första kod i PHP
     echo 'Hello, World';
     ?>
+
+<?php
+$tal1 = 234;
+$tal2 = 923;
+
+$produkt = $tal1 * $tal2;
+
+echo "Produkten av $tal1 och $tal2 är: $produkt";
+?>
 </body>
 </html>
