49fdaa23e34417f51be70d4ff2f86fc892a776d5
Eyad <eyad.hosain.2017@gmail.com>
2024-11-27 09:31:22 +0100

    init

---
+++ src/lektion02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lektion02 - PHP: Datatyper, Arrayer och Kontrollstrukturer</title>
+    <style>
+        *{
+            font-family: 'Verdana', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>Lektion02 - PHP: Datatyper, Arrayer och Kontrollstrukturer</h1>
+    <h2>Att skriva PHP</h2>
+    <p>
+    <?php
+    echo 'Detta är en text från ett php-block';
+    ?>
+    <?= 'Detta är också en text från php'?> 
+    </p>
+</body>
+</html>
