aa1f196d2cd97416b82064a288f9a638a01144d7
Eyad <eyad.hosain.2017@gmail.com>
2024-12-03 10:38:06 +0100

    init

---
+++ src/Lektion03/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lektion03 - Kontrollstrukturer, Funktioner , objekt och klass</title>
+</head>
+<body>
+    <h1>Lektion03 - Kontrollstrukturer, Funktioner , objekt och klass</h1>;
+    <h3>If, else, if-else satser</h3>
+
+
+
+</body>
+</html>
--- src/lektion02/index.php
+++ src/lektion02/index.php
@@ -36,11 +36,94 @@
         ?>
     </ul>
     <?php
-    // skillnad på " eller ' ?
-    echo "sträng som har en variabel i sig ($integer)<br>"
+        // Skillnad på " eller ' ? 
 
-    $x = 5;
-    echo "X ($x) är :"   . gettype($x) . "<br>";
+        // " - skriver ut variabler
+        // " - escape:are tecken
+
+        echo "Sträng som har en variabel i sig ($integer)<br>";
+        echo "Sträng som innehåller en escape:ad radbrytnign \n\t men detta syns inte i html.<br>";
+
+        $x = 5; 
+        echo "X ($x) är: " . gettype($x) . "<br>";
+        settype($x, 'string');
+        echo "X ($x) är: " . gettype($x) . "<br>";
+        echo "Finns Y? " . isset($y) . "<br>";
+        echo "Finns X? " . isset($x) . "<br>";
+
+        $y = 'Kalle';
+        unset($y);
+        echo "Y? ($y)<br>";
+
+        // Dynamiska variabler "made simple" ;-) 
+        $nivå1 = 'nivå2';
+        echo "\$nivå1 = $nivå1<br>";
+        $$nivå1 = 'nivå3';
+        echo "\$nivå2 = $nivå2<br>";
+        $$$nivå1 = 'nivå4';
+        echo "\$nivå3 = $nivå3<br>";
+
+        $dog = 'Är en hund';
+        $cat = &$dog;
+        echo "\$dog = $dog, \$cat = $cat<br>";
+        $cat = 'Är ju en katt';
+        echo "\$dog = $dog, \$cat = $cat<br>";
+
+        // Definera en ny konstant
+        define('PI', 3.14159);
+        echo "Vi kan nu använda PI i våra beräkningar " . PI . "<br>";
     ?>
+    </p>
+
+<h2>Listor (arrayer)</h2>
+
+$nummer = [ 1, 6, 9, 2, 9, 5 ];<br>
+
+<?php
+    $nummer = [ 1, 6, 9, 2, 9, 5 ];
+    echo "På index nummer fyra finns: $nummer[4]<br>"
+?>
+
+print_r($nummer); -- kan vara bra för felsökning<br>
+<?php
+    print_r($nummer);
+?>
+
+<h3>En associativ lista (nycklar)</h3>
+
+<?php
+
+    $bil = [
+        'brand' => 'Suzuki',
+        'model' => 'Samurai',
+        'year' => 1986,
+        'bestCar' => true
+    ];
+    
+?>
+Världens bästa bil är en <?= $bil['brand'] ?> <?= $bil['model'] ?>! <br>
+
+<h3>LIFO-kö</h3>
+LIFO = First In First Out<br>
+<?php
+    $fifo = [];
+
+    array_push($fifo, $bil);
+    array_push($fifo, 'En Volvo');
+    print_r($fifo);
+
+    $sistaElementet = array_pop($fifo);
+    echo "Vi hämtade $sistaElementet<br>";
+    print_r($fifo);
+
+?>
+
+<h2>Super Globaler</h2>
+
+$_SERVER, $GET
+
+<?php
+    print_r($_GET);
+?>
 </body>
 </html>
